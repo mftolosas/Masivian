2e4fbceff6900f256c3bb6cb0408eb5a80a738e9
Maria Fernanda Tolosa <desarrollador4@azsmart.net>
2020-08-23 20:56:07 -0500

    Add endpoints to create and open a roulette

---
+++ RouletteAPI/Controllers/RouletteController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using RouletteAPI.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RouletteController : ControllerBase
+    {
+        static ConcurrentBag<Roulette> roulettes = new ConcurrentBag<Roulette>();
+
+        [HttpPost]
+        [Route("SetRoulette")]
+        public int SetRoulette(Roulette roulette)
+        {
+            try
+            {
+                if (roulette.Name != null)
+                {
+                    roulette.Id = roulettes.Count() + 1;
+                    roulettes.Add(roulette);
+                }
+                else
+                {
+                    return 0;
+                }
+                return roulette.Id;
+            }
+            catch
+            {
+                return 0;
+            }      
+        }
+
+        [HttpPost]
+        [Route("OpenRoulette")]
+        public IActionResult OpenRoulette(int id)
+        {
+            try
+            {
+                if (roulettes.Any(i => i.Id == id))
+                {
+                    roulettes.Where(i => i.Id == id).FirstOrDefault(v => v.Status = true);
+                }
+                else
+                {
+                    return BadRequest($"La ruleta con el ID {id} no fue encontrada, verifique e intente nuevamente");
+                }
+                return StatusCode(200);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ RouletteAPI/Models/Roulette.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Models
+{
+    public class Roulette
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; }
+        public bool Status { get; set; } = false;
+    }
+}
